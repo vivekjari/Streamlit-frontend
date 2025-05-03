723568decb1ba296d52cf7cfc038e858de273fdc
Vivek Jariwala <110350881+vivekjari@users.noreply.github.com>
2025-05-03 14:32:35 +0530

    Add files via upload

--- chat_engine.py
+++ chat_engine.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
+from langchain_community.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
 from dotenv import load_dotenv
 
